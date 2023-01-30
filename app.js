28b38cb600d22486ccdde98128075f00664ace83
Jacky Hung <jacky85725@gmail.com>
2023-01-31 01:05:34 +0800

    get index to work properly

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const exhbs = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
 app.engine('handlebars',exhbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('index')
