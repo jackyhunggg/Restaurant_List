220c4ddf8add2cb01c8decdc447b5034fac67552
Jacky Hung <jacky85725@gmail.com>
2023-01-31 03:03:54 +0800

    finish index

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const express = require('express');
 const exhbs = require('express-handlebars');
 const app = express();
 const port = 3000;
+const list = require('./restaurant.json')
 
 app.use(express.static('public'));
 app.engine('handlebars',exhbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {restaurant: list.results});
+    console.log(list.results)
 })
 app.listen(port, () => {
     console.log(`express is running on http:// localhost: ${port}`)
