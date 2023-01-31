33a021f72dea5ff2ff98a425eecbf4bad5ed4252
Jacky Hung <jacky85725@gmail.com>
2023-02-01 01:43:46 +0800

    add some comments

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ app.use(express.static('public'));
 app.engine('handlebars',exhbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// 使用者可以在首頁看到所有餐廳與它們的簡單資料
 app.get('/', (req, res) => {
     res.render('index', {restaurant: list.results});
 })
 
+// 使用者可以透過搜尋餐廳名稱來找到特定的餐廳
 app.get('/search', (req,res) => {
     const keyword = req.query.keyword;
     const restaurants = list.results.filter(restaurant => {
@@ -21,6 +23,7 @@ app.get('/search', (req,res) => {
     res.render('index', {restaurants: restaurants, keyword: keyword})
 })
 
+// 使用者可以點進去看餐廳的詳細資訊
 app.get('/:restaurant_id', (req, res) => {
     // console.log('restaurant id: ', typeof(req.params.restaurant_id))
     const restaurant = list.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
