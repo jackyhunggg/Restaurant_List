da1a21725ce129a8a2dae83df3a38b9d178c5031
Jacky Hung <jacky85725@gmail.com>
2023-02-01 00:20:35 +0800

    finish show

--- app.js
+++ app.js
@@ -10,8 +10,16 @@ app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
     res.render('index', {restaurant: list.results});
-    console.log(list.results)
+    // console.log(list.results)
 })
+
+app.get('/:restaurant_id', (req, res) => {
+    // console.log('restaurant id: ', typeof(req.params.restaurant_id))
+    const restaurant = list.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+    console.log(req.params.restaurant_id.toString())
+    res.render('show', {restaurant: restaurant})
+})
+
 app.listen(port, () => {
     console.log(`express is running on http:// localhost: ${port}`)
 })
